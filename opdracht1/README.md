8f693c6f8d205828c20aa8769a21b3c529a0bc99
frederiqueisabel <70712050+frederiqueisabel@users.noreply.github.com>
2021-02-17 11:55:33 +0100

    Opdracht 1 Microinteractie
    Microinteractie opdracht aanpassingen.

--- opdracht1/README.md
+++ opdracht1/README.md
@@ -6,12 +6,14 @@ Lees hier de [opdrachtbeschrijving](./opdrachtbeschrijving.md).
 
 
 # Project titel
-Beschrijf hier je eigen project.
+Mijn project heet Theatr. Het is een movie streaming website, waar je films kan toevoegen aan je favorieten, zodat je deze later terug kan vinden.
 En voeg een link naar je demo toe.
 
 ## interface
 Leg de interface uit. In de demo heb je de interface design principles 04 & 11 van [Principles of User Interface Design](http://bokardo.com/principles-of-user-interface-design/) toegepast. Hoe heb je dat gedaan?
 
+De gebruiker is in control, omdat de gebruiker zelf kan zoeken naar films en films zelf kan opslaan naar de favorietenlijst.
+De hierarchie heb ik aangegeven door middel van kleur en grootte. Zo heeft het logo een redelijk opvallende kleur en het is redelijk groot. Daar zal je oog waarschijnlijk eerst langsgaan. Daarna gaat het al snel naar de plaatjes van de films.
 
 ## code
-Leg de code uit.
+Ik heb een section met een unordered list gemaakt voor elke horizontale lijst met films. Elke film is een li, waar een img voor de cover in zit en een img voor het favorietenhartje. Met een JavaScript functie heb ik ervoor gezorgd dat de src van de img van het hartje verandert als je erop klikt. Als je dubbelklikt, wordt het hartje weer leeg.
--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -4,3 +4,314 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+var klikFavo = document.querySelector("#hartje1");
+
+klikFavo.addEventListener("click", animatieKlik);
+
+function animatieKlik () {
+  console.log("hello");
+ 
+  klikFavo.src = "images/hartvol.png";
+}
+klikFavo.addEventListener("dblclick", animatieKlik2);
+
+function animatieKlik2 () {
+  console.log("hello");
+ 
+ klikFavo.src = "images/hart.png"
+}
+ 
+
+
+var klikFavo2 = document.querySelector("#hartje2");
+
+klikFavo2.addEventListener("click", animatieKlikaan2 );
+
+function animatieKlikaan2 () {
+  console.log("hello");
+ 
+  klikFavo2.src = "images/hartvol.png";
+}
+klikFavo2.addEventListener("dblclick", animatieKlikuit2);
+
+function animatieKlikuit2 () {
+  console.log("hello");
+ 
+ klikFavo2.src = "images/hart.png"
+}
+
+
+
+var klikFavo3 = document.querySelector("#hartje3");
+
+klikFavo3.addEventListener("click", animatieKlikaan3 );
+
+function animatieKlikaan3 () {
+  console.log("hello");
+ 
+  klikFavo3.src = "images/hartvol.png";
+}
+klikFavo3.addEventListener("dblclick", animatieKlikuit3);
+
+function animatieKlikuit3 () {
+  console.log("hello");
+ 
+ klikFavo3.src = "images/hart.png"
+}
+
+
+var klikFavo4 = document.querySelector("#hartje4");
+
+klikFavo4.addEventListener("click", animatieKlikaan4 );
+
+function animatieKlikaan4 () {
+  console.log("hello");
+ 
+  klikFavo4.src = "images/hartvol.png";
+}
+klikFavo4.addEventListener("dblclick", animatieKlikuit4);
+
+function animatieKlikuit4 () {
+  console.log("hello");
+ 
+ klikFavo4.src = "images/hart.png"
+}
+
+var klikFavo5 = document.querySelector("#hartje5");
+
+klikFavo5.addEventListener("click", animatieKlikaan5 );
+
+function animatieKlikaan5 () {
+  console.log("hello");
+ 
+  klikFavo5.src = "images/hartvol.png";
+}
+klikFavo5.addEventListener("dblclick", animatieKlikuit5);
+
+function animatieKlikuit5 () {
+  console.log("hello");
+ 
+ klikFavo5.src = "images/hart.png"
+}
+
+var klikFavo6 = document.querySelector("#hartje6");
+
+klikFavo6.addEventListener("click", animatieKlikaan6 );
+
+function animatieKlikaan6 () {
+  console.log("hello");
+ 
+  klikFavo6.src = "images/hartvol.png";
+}
+klikFavo6.addEventListener("dblclick", animatieKlikuit6);
+
+function animatieKlikuit6 () {
+  console.log("hello");
+ 
+ klikFavo6.src = "images/hart.png"
+}
+
+var klikFavo7 = document.querySelector("#hartje7");
+
+klikFavo7.addEventListener("click", animatieKlikaan7 );
+
+function animatieKlikaan7 () {
+  console.log("hello");
+ 
+  klikFavo7.src = "images/hartvol.png";
+}
+klikFavo7.addEventListener("dblclick", animatieKlikuit7);
+
+function animatieKlikuit7 () {
+  console.log("hello");
+ 
+ klikFavo7.src = "images/hart.png"
+}
+
+var klikFavo8 = document.querySelector("#hartje8");
+
+klikFavo8.addEventListener("click", animatieKlikaan8 );
+
+function animatieKlikaan8 () {
+  console.log("hello");
+ 
+  klikFavo8.src = "images/hartvol.png";
+}
+klikFavo8.addEventListener("dblclick", animatieKlikuit8);
+
+function animatieKlikuit8 () {
+  console.log("hello");
+ 
+ klikFavo8.src = "images/hart.png"
+}
+
+var klikFavo9 = document.querySelector("#hartje9");
+
+klikFavo9.addEventListener("click", animatieKlikaan9 );
+
+function animatieKlikaan9 () {
+  console.log("hello");
+ 
+  klikFavo9.src = "images/hartvol.png";
+}
+klikFavo9.addEventListener("dblclick", animatieKlikuit9);
+
+function animatieKlikuit9 () {
+  console.log("hello");
+ 
+ klikFavo9.src = "images/hart.png"
+}
+
+var klikFavo10 = document.querySelector("#hartje10");
+
+klikFavo10.addEventListener("click", animatieKlikaan10 );
+
+function animatieKlikaan10 () {
+  console.log("hello");
+ 
+  klikFavo10.src = "images/hartvol.png";
+}
+klikFavo10.addEventListener("dblclick", animatieKlikuit10);
+
+function animatieKlikuit10 () {
+  console.log("hello");
+ 
+ klikFavo10.src = "images/hart.png"
+}
+
+var klikFavo11 = document.querySelector("#hartje11");
+
+klikFavo11.addEventListener("click", animatieKlikaan11 );
+
+function animatieKlikaan11 () {
+  console.log("hello");
+ 
+  klikFavo11.src = "images/hartvol.png";
+}
+klikFavo11.addEventListener("dblclick", animatieKlikuit11);
+
+function animatieKlikuit11 () {
+  console.log("hello");
+ 
+ klikFavo11.src = "images/hart.png"
+}
+
+var klikFavo12 = document.querySelector("#hartje12");
+
+klikFavo12.addEventListener("click", animatieKlikaan12 );
+
+function animatieKlikaan12 () {
+  console.log("hello");
+ 
+  klikFavo12.src = "images/hartvol.png";
+}
+klikFavo12.addEventListener("dblclick", animatieKlikuit12);
+
+function animatieKlikuit12 () {
+  console.log("hello");
+ 
+ klikFavo12.src = "images/hart.png"
+}
+
+var klikFavo13 = document.querySelector("#hartje13");
+
+klikFavo13.addEventListener("click", animatieKlikaan13 );
+
+function animatieKlikaan13 () {
+  console.log("hello");
+ 
+  klikFavo13.src = "images/hartvol.png";
+}
+klikFavo13.addEventListener("dblclick", animatieKlikuit13);
+
+function animatieKlikuit13 () {
+  console.log("hello");
+ 
+ klikFavo13.src = "images/hart.png"
+}
+
+var klikFavo14 = document.querySelector("#hartje14");
+
+klikFavo14.addEventListener("click", animatieKlikaan14 );
+
+function animatieKlikaan14 () {
+  console.log("hello");
+ 
+  klikFavo14.src = "images/hartvol.png";
+}
+klikFavo14.addEventListener("dblclick", animatieKlikuit14);
+
+function animatieKlikuit14 () {
+  console.log("hello");
+ 
+ klikFavo14.src = "images/hart.png"
+}
+
+var klikFavo15 = document.querySelector("#hartje15");
+
+klikFavo15.addEventListener("click", animatieKlikaan15 );
+
+function animatieKlikaan15 () {
+  console.log("hello");
+ 
+  klikFavo15.src = "images/hartvol.png";
+}
+klikFavo15.addEventListener("dblclick", animatieKlikuit15);
+
+function animatieKlikuit15 () {
+  console.log("hello");
+ 
+ klikFavo15.src = "images/hart.png"
+}
+
+var klikFavo16 = document.querySelector("#hartje16");
+
+klikFavo16.addEventListener("click", animatieKlikaan16 );
+
+function animatieKlikaan16 () {
+  console.log("hello");
+ 
+  klikFavo16.src = "images/hartvol.png";
+}
+klikFavo16.addEventListener("dblclick", animatieKlikuit16);
+
+function animatieKlikuit16 () {
+  console.log("hello");
+ 
+ klikFavo16.src = "images/hart.png"
+}
+
+var klikFavo17 = document.querySelector("#hartje17");
+
+klikFavo17.addEventListener("click", animatieKlikaan17 );
+
+function animatieKlikaan17 () {
+  console.log("hello");
+ 
+  klikFavo17.src = "images/hartvol.png";
+}
+klikFavo17.addEventListener("dblclick", animatieKlikuit17);
+
+function animatieKlikuit17 () {
+  console.log("hello");
+ 
+ klikFavo17.src = "images/hart.png"
+}
+
+var klikFavo18 = document.querySelector("#hartje18");
+
+klikFavo18.addEventListener("click", animatieKlikaan18 );
+
+function animatieKlikaan18 () {
+  console.log("hello");
+ 
+  klikFavo18.src = "images/hartvol.png";
+}
+klikFavo18.addEventListener("dblclick", animatieKlikuit18);
+
+function animatieKlikuit18 () {
+  console.log("hello");
+ 
+ klikFavo18.src = "images/hart.png"
+}
